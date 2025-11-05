b71c14c1c30ca2bd258dc99c6f38df4f4e119707
wallacenrv <Wallacen.ribeiro@gmail.com>
2025-11-05 16:06:10 -0300

    commit inicial

---
+++ src/test/java/com/wallacen/notificacao/NotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallacen.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
